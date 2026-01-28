14e0c914be26de3c2f99d2ce8448301e90bc79c1
M.Pozd <mpozd237@gmail.com>
2026-01-28 11:33:45 +0200

    Fixing version - Tests passed

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -77,7 +77,6 @@ public class SauceDemoTest {
         //Assert.assertEquals("1", headerPage.getShoppingCarBadge().getText());
 
     }
-
     //Closing Chrome windows after ending test with any result - pass /failed
     @AfterMethod
     public void tearDown() {
